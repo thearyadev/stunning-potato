b23b511e9a2fa4734cd151fc6eec49bbab59fd76
thearyadev <personal@aryankothari.dev>
2023-05-04 14:50:08 -0400

    changes

--- util/database/database_access.py
+++ util/database/database_access.py
@@ -1,4 +1,6 @@
 import psycopg2
+from pathlib import Path
+
 
 class DatabaseAccess:
     def __init__(self, db_name, db_user, db_password, db_host, db_port):
@@ -7,16 +9,22 @@ class DatabaseAccess:
         self.db_password = db_password
         self.db_host = db_host
         self.db_port = db_port
-        
+
         self.connection = psycopg2.connect(
             host=self.db_host,
             port=self.db_port,
             dbname=self.db_name,
             user=self.db_user,
-            password=self.db_password
+            password=self.db_password,
         )
 
+    def initialize(self, sql_path: Path):
+        with open(sql_path, "r") as sql_file, self.connection.cursor() as cursor:
+            cursor.execute(sql_file.read())
+            self.connection.commit()
+
 if __name__ == "__main__":
-    print("balls")
-    db_access = DatabaseAccess("lewdlocale_test2", "postgre", "", "localhost", "5432")
-    print(db_access.connection.cursor())
+    db_access = DatabaseAccess(
+        "lewdlocale", "lewdlocale", "lewdlocale", "localhost", "5432"
+    )
+    db_access.initialize(Path("util/database/tables.sql"))
