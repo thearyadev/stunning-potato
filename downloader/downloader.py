c9854abddffa5f2ad7a8ee341ab330141b7c8714
thearyadev <personal@aryankothari.dev>
2023-05-12 18:07:50 -0400

    changes

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -11,24 +11,46 @@ from util.models.film import FilmStateEnum
 import logging
 from beartype import beartype
 import urllib.request
+import os
+from pathlib import Path
+from uuid import UUID
+
 
 @beartype
 class Downloader:
     def __init__(self, databaseAccess: DatabaseAccess):
         self.db = databaseAccess
+        self.download_path = Path(os.getenv("DOWNLOAD_PATH"))
 
     def main_loop(self):
         while True:
-            queue_item = self.db.get_and_pop_queue() # get and pop from queue 
+            queue_item = self.db.get_and_pop_queue()  # get and pop from queue
             if queue_item is not None:
                 # get download url
-                self.db.set_film_state(queue_item.film_uuid, new_state=FilmStateEnum.DOWNLOADING)# set_state to DOWNLOADING
-                self.download(get_download_url(get_document(get_iframe_source(get_document(queue_item.url))))) # begin downloading
-                # complete download 
-                self.db.set_film_state(queue_item.film_uuid, new_state=FilmStateEnum.COMPLETE)# set_state to COMPLETE
+                self.db.set_film_state(
+                    queue_item.film_uuid, new_state=FilmStateEnum.DOWNLOADING
+                )  # set_state to DOWNLOADING
+                self.download(
+                    get_download_url(
+                        get_document(get_iframe_source(get_document(queue_item.url)))
+                    ),
+                    os.path.join(self.download_path, f"this is a uuid.mp4"),
+                )  # begin downloading
+                # complete download
+                self.db.set_film_state(
+                    queue_item.film_uuid, new_state=FilmStateEnum.COMPLETE
+                )  # set_state to COMPLETE
             time.sleep(5)
+
     
-    def download(self, url: str) -> None:
-        # db.set_download_progress at 25, 75, and 100
-        time.sleep(5)
-        logging.info(f"Downloading {url}")
+
+    def download(self, url: str, filename, uuid: UUID) -> None:
+
+        def report_hook(count: int, block_size: int, total_size: int):
+            # tells db to do the thing
+            progress: float = count * block_size * 100 / total_size
+            if progress % 25 == 0:
+                # self.db.set_film_progress(uuid, progress)
+                ...
+
+        urllib.request.urlretrieve(url, filename, reporthook=report_hook)
--- launch.py
+++ launch.py
@@ -2,8 +2,10 @@ import argparse
 from indexer.indexer import Indexer
 from downloader.downloader import Downloader
 from util.database.database_access import DatabaseAccess
+from util.config import load_config
 from pathlib import Path
 import logging
+from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser(description='pogger')
 parser.add_argument(
@@ -13,10 +15,12 @@ parser.add_argument("--app", type=str, help="app name", choices=["server", "down
 parser.add_argument("--flush", type=str, help="flush database", default="True")
 args = parser.parse_args()
 
+load_dotenv()
+
 logging.basicConfig(level=args.log_level)
+config: dict = load_config(Path("./config.yaml"))
 
-
-db = DatabaseAccess("lewdlocale", "lewdlocale", "lewdlocale", "localhost", 5432)
+db = DatabaseAccess(config.get("DB_USER"), config.get("DB_PASS"), config.get("DB_NAME"), config.get("DB_HOST"), config.get("DB_PORT"))
 db.initialize(Path("./util/database/tables.sql"))
 
 if args.flush == "True":
