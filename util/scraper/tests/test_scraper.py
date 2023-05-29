085bf695815c94cdff17cdabda5c625800f46847
thearyadev <personal@aryankothari.dev>
2023-05-29 13:34:02 -0400

    init test file

---
+++ util/scraper/tests/test_scraper.py
@@ -0,0 +1,11 @@
+import json
+from util.scraper.detail_page import *
+from util.scraper.document import *
+from util.scraper.list_page import *
+
+answers: dict = {"answer": "42"}
+
+def test_deez():
+    # Test deez nuts
+    print(answers['answer'])
+    assert True
