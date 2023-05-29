2612546c7de57752e45a47ac63eca6e475705547
thearyadev <personal@aryankothari.dev>
2023-05-29 13:33:44 -0400

    create answer key

---
+++ util/scraper/tests/ans-gen.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.path.append(".")  # just so i can run this from root
+
+from util.scraper.detail_page import *
+from util.scraper.document import *
+from util.scraper.list_page import *
+
+from indexer.indexer import build_url, extract_film_id
+
+answers: dict = {
+
+}
+
+TARGET_URL = "https://hqporner.com/"
+
+def main():
+    urls: list[str] = collect_urls(get_document(TARGET_URL))
+    for url in urls: 
+        document = get_document(build_url(extract_film_id(url)))
+        title = get_film_title(document)
+        actresses = get_film_actresses(document)
+        duration = get_film_duration(document)
+        iframe_src = get_iframe_source(document)
+        download_url = get_download_url(get_document(iframe_src))
+
+        answers[url] = {
+            "title": title,
+            "actresses": actresses,
+            "duration": duration.seconds,
+            "iframe_src": iframe_src,
+            "download_url": download_url
+        }
+    with open("answers.json", "w") as f:
+        json.dump(answers, f)
+
+if __name__ == "__main__":
+    main()
