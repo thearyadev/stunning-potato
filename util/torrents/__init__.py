20e10bd7acb3e46e99cabf4c32340991dd85183e
Aryan Kothari <personal@aryankothari.dev>
2023-11-23 03:05:33 +0000

    clear file

--- util/torrents/__init__.py
+++
@@ -1,9 +0,0 @@
-from torrentool.api import Torrent
-
-
-def get_video_file_list(torrent_file_contents: bytes) -> list[str]:
-    return [
-        f.name
-        for f in Torrent.from_string(torrent_file_contents).files
-        if f.name.endswith("mp4") or f.name.endswith("mkv")
-    ]
